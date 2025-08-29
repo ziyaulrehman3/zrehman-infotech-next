ecb2a1429f6dcc2aa2214ab2aeb632792f431d1a
Ziya Ul Rehman <ziyaulrehman3@gmail.com>
2025-08-30 00:59:43 +0530

    ok

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -17,7 +17,7 @@ export default function ContactUs() {
       <div className="w-full py-12 px-6 lg:px-12  lg:py-20 text-black bg-white flex flex-col items-center">
         <h1 className="text-3xl lg:text-5xl font-bold flex flex-col gap-2 text-bold text-center py-2">
           <span> Have an Idea in mind?</span>
-          <span className="text-[#FE743C]">Let's Start a Conversation</span>
+          <span className="text-[#FE743C]">{"Let's Start a Conversation"}</span>
         </h1>
 
         <p className="text-gray-600 w-full lg:w-1/2 text-center">
